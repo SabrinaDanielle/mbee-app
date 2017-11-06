7e9bf113f50f6eb8dcdc8a5527c42487c25d2039
brinadanielle <bri.daniG@gmail.com>
2017-11-06 16:15:21 -0600

    added userIsAdmin

--- src/utils/router.js
+++ src/utils/router.js
@@ -5,6 +5,7 @@ import { pathToJS } from 'react-redux-firebase'
 import LoadingSpinner from 'components/LoadingSpinner'
 import { get } from 'lodash'
 import CircularProgress from 'material-ui/CircularProgress'
+import React from 'react'
 
 const AUTHED_REDIRECT = 'AUTHED_REDIRECT'
 const UNAUTHED_REDIRECT = 'UNAUTHED_REDIRECT'
@@ -45,7 +46,7 @@ export const UserIsNotAuthenticated = UserAuthWrapper({
   // eslint-disable-line new-cap
   wrapperDisplayName: 'UserIsNotAuthenticated',
   allowRedirectBack: false,
-  LoadingComponent: LoadingSpinner,
+  LoadingComponent: CircularProgress,
   failureRedirectPath: (state, props) =>
     // redirect to page user was on or to list path
     props.location.query.redirect || LIST_PATH,
@@ -67,10 +68,34 @@ export const UserIsNotAuthenticated = UserAuthWrapper({
  * @param {Component} componentToWrap - Component to wrap
  * @return {Component} wrappedComponent
  */
+export const UserIsAdmin = UserAuthWrapper({
+  // eslint-disable-line new-cap
+  authSelector: ({ firebase: { profile, auth } }) => ({ auth, profile }),
+  authenticatingSelector: ({ firebase: { profile, auth, isInitializing } }) =>
+    auth === undefined || profile === undefined || isInitializing === true,
+  redirectAction: newLoc => dispatch => {
+    browserHistory.replace(newLoc)
+    dispatch({ type: UNAUTHED_REDIRECT })
+  },
+  allowRedirectBack: false,
+  failureRedirectPath: '/login',
+  wrapperDisplayName: 'UserIsAdmin',
+  predicate: auth => get(auth, `profile.role.name`) === 'admin',
+  LoadingComponent: <CircularProgress mode="indeterminate" size={80} />
+})
+
+/**
+ * @description Higher Order Component that redirects to the homepage if
+ * the user does not have the required permission. This HOC requires that the user
+ * profile be loaded and the role property populated
+ * @param {Component} componentToWrap - Component to wrap
+ * @return {Component} wrappedComponent
+ */
+
 export const UserHasPermission = permission =>
   UserAuthWrapper({
     wrapperDisplayName: 'UserHasPermission',
-    LoadingComponent: CircularProgress,
+    LoadingComponent: LoadingScreen,
     allowRedirectBack: false,
     failureRedirectPath: '/login',
     authSelector: ({ firebase: { profile, auth } }) => ({ auth, profile }),
@@ -85,5 +110,7 @@ export const UserHasPermission = permission =>
 
 export default {
   UserIsAuthenticated,
-  UserIsNotAuthenticated
+  UserIsNotAuthenticated,
+  UserIsAdmin,
+  UserHasPermission
 }
